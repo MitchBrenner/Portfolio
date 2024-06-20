2f8ebb19970d17bd8526be4f0f2eec0a79e38859
Mitchell Brenner <mitchellbrenner@Mitchells-MacBook-Pro.local>
2024-06-20 14:22:54 -0500

    Updated for Mobile

--- components/About.tsx
+++ components/About.tsx
@@ -1,67 +1,72 @@
-import React from 'react'
-import { motion } from 'framer-motion'
-import { PageInfo } from '@/typings'
+import React from "react";
+import { motion } from "framer-motion";
+import { PageInfo } from "@/typings";
 
 type Props = {
-    pageInfo: PageInfo
-}
+  pageInfo: PageInfo;
+};
 
 export default function About({ pageInfo }: Props) {
-    return (
-        <div className='h-screen flex flex-col relative text-center 
+  return (
+    <div
+      className="h-screen flex flex-col relative text-center 
         md:text-left md:flex-row max-w-6xl px-10 justify-evenly mx-auto 
-        items-center'>
-                <motion.h3 
-                    initial={{
-                        opacity: 0,
-                    }}
-                    whileInView={{
-                        opacity: 1
-                    }}
-                    transition={{
-                        duration: 1.3
-                    }}
-                    className='subTitle'>About</motion.h3>
+        items-center pb-10 md:pb-0"
+    >
+      <motion.h3
+        initial={{
+          opacity: 0,
+        }}
+        whileInView={{
+          opacity: 1,
+        }}
+        transition={{
+          duration: 1.3,
+        }}
+        className="subTitle"
+      >
+        About
+      </motion.h3>
 
-                <motion.img 
-                    initial={{
-                        x: -200,
-                        opacity: 0
-                    }}
-                    transition={{
-                        duration: 1.0
-                    }}
-                    whileInView={{
-                        x: 0,
-                        opacity: 1
-                    }}
-                    // viewport={{ once: true }}
-                    src='/aboutpic.JPG' 
-                    className='mt-10 -mb-[80px] md:mb-0 flex-shrink-0 w-56 h-56 rounded-full 
+      <motion.img
+        initial={{
+          x: -200,
+          opacity: 0,
+        }}
+        transition={{
+          duration: 1.0,
+        }}
+        whileInView={{
+          x: 0,
+          opacity: 1,
+        }}
+        // viewport={{ once: true }}
+        src="/aboutpic.JPG"
+        className="mt-10 -mb-[80px] md:mb-0 flex-shrink-0 w-48 h-48 rounded-full 
                     object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[400px] xl:h-[500px] 
-                    xl:object-cover'
-                />
-                <motion.div 
-                    initial={{
-                        x: 300,
-                        opacity: 0
-                    }}
-                    transition={{
-                        duration: 1.0
-                    }}
-                    whileInView={{
-                        x: 0,
-                        opacity: 1
-                    }}
-                    className='space-y-6 px-0 md:px-10'
-                >
-                    <h4 className='text-4xl font-semibold'>
-                        Here is a <span className='underline decoration-[#9370DB]/50'>little</span> background
-                    </h4>
-                    <p className='text-sm'>
-                        {pageInfo?.backgroundInformation}
-                    </p>
-                </motion.div>
-        </div>
-    )
+                    xl:object-cover"
+      />
+      <motion.div
+        initial={{
+          x: 300,
+          opacity: 0,
+        }}
+        transition={{
+          duration: 1.0,
+        }}
+        whileInView={{
+          x: 0,
+          opacity: 1,
+        }}
+        className="space-y-6 px-0 md:px-10"
+      >
+        <h4 className="text-3xl sm:text-4xl font-semibold">
+          Here is a{" "}
+          <span className="underline decoration-[#9370DB]/50">little</span>{" "}
+          background
+        </h4>
+        <p className="text-sm">{pageInfo?.backgroundInformation}</p>
+      </motion.div>
+    </div>
+  );
 }
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,44 +1,43 @@
-import React from 'react'
-import { motion } from 'framer-motion'
-import Skill from './Skill'
-import { Technology } from '@/typings'
+import React from "react";
+import { motion } from "framer-motion";
+import Skill from "./Skill";
+import { Technology } from "@/typings";
 
 type Props = {
-    skills: Technology[]
-}
+  skills: Technology[];
+};
 
 export default function Skills({ skills }: Props) {
   return (
-    <motion.div 
-        initial={{
-            opacity: 0,
-        }}
-        whileInView={{
-            opacity: 1,
-        }}
-        transition={{
-            duration: 1.3
-        }}
-        className='flex relative flex-col text-center md:text-left xl:flex-row
+    <motion.div
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1.3,
+      }}
+      className="flex relative flex-col text-center md:text-left xl:flex-row
         max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto
-        items-center overflow-visible'>
-        <h3 className='subTitle'>Skills</h3>
-        <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>Hover over a skill for current proficiency</h3>
+        items-center overflow-visible"
+    >
+      <h3 className="subTitle">Skills</h3>
+      <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-xs px-5 sm:text-sm">
+        Hover over skills
+      </h3>
 
-        <div className='grid grid-cols-3 gap-5 pt-[100px] sm:pt-[150px] sm:grid-cols-4 sm:gap-5'>
-
-            {
-                skills?.slice(0, 8).map((skill) => (
-                    <Skill key={skill._id} skill={skill}/>
-                ))
-            }
-            {
-                skills?.slice(8, skills.length).map((skill) => (
-                    <Skill key={skill._id} skill={skill} directionLeft/>
-                ))
-            }
-
-        </div>
+      <div className="grid grid-cols-3 gap-5 pt-[40px] sm:pt-[100px] sm:grid-cols-4 sm:gap-5">
+        {skills
+          ?.slice(0, 8)
+          .map((skill) => <Skill key={skill._id} skill={skill} />)}
+        {skills
+          ?.slice(8, skills.length)
+          .map((skill) => (
+            <Skill key={skill._id} skill={skill} directionLeft />
+          ))}
+      </div>
     </motion.div>
-  )
+  );
 }
